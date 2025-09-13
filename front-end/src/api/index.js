9ea07bd77c3ea6ba86d59a73329983fab433177b
avytran <tr.anhvy.work@gmail.com>
2025-09-13 09:41:39 +0700

    feat: landing page routing and api index file

---
+++ front-end/src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const ENV = process.env.REACT_APP_ENV.trim() || "development";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+let api;
+
+if (ENV === 'local'){
+    api = {
+        get: async (url) => {
+            if (url !== ""){
+                const mockData = require(`../mocks/data${url}.json`);
+                return { data: { data: mockData }} ;
+            }            
+            return { data: {data: []} };
+        }
+    }
+} else {
+    api = axios.create({
+        baseURL: API_BASE_URL,
+        timeout: 10000,
+        headers: {"Content-Type": "application/json"}
+    })
+}
+
+export { api };
