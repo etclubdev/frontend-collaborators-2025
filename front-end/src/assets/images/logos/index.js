04f4e796e23e54c0894ec4d9d4c27c5f81a543b4
Anh Vy Tran <tr.anhvy.work@gmail.com>
2025-09-14 17:34:46 +0700

    Feature/Create React App (#1)
    * feat: init react app
    
    * feat: add assets
    
    * feat: landing page routing and api index file

---
+++ front-end/src/assets/images/logos/index.js
@@ -0,0 +1,6 @@
+import horizontalLogo from './horizontalLogo.png';
+import textLogo from './textLogo.png';
+import verticalLogo from './verticalLogo.png';
+import noTextLogo from './noTextLogo.png';
+
+export { textLogo, horizontalLogo, verticalLogo, noTextLogo, };
